81b18ef92d50f79bc80a367d65c23da069157a96
An Go <gorbunoochek@gmail.com>
2024-02-14 20:04:45 +0100

    notes routes init

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,15 +1,62 @@
 const router = require("express").Router()
-
-// aut
-
 const Note = require("../models/Notes.model")
+const requireAuth = require("../middleware/requireAuth")
 
-router.post("/", async (req, res, next) => {
+router.post("/", requireAuth, async (req, res, next) => {
   try {
-    const userId = req.user._id
     const { text } = req.body
-    const createdNote = await Note.create({ text })
+    const createdNote = await Note.create({ text, user: req.user._id })
     res.status(201).json({ message: "Note created", data: createdNote })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/", requireAuth, async (req, res, next) => {
+  try {
+    const notes = await Note.find({ user: req.user._id })
+      .select({ text: 1 })
+      .populate("user", { _id: 0, email: 1 })
+    res.status(200).json({ message: "All notes successfully found", notes })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get("/:noteId", async (req, res, next) => {
+  const noteId = req.params.noteId
+  try {
+    const note = await Note.findById(noteId)
+      .select({ text: 1 })
+      .populate("user", { _id: 0, email: 1 })
+    res.status(200).json({ message: "Note found by ID", data: note })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put("/:noteId", async (req, res, next) => {
+  const noteId = req.params.noteId
+  const { text } = req.body
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(
+      noteId,
+      { text },
+      { new: true }
+    ).populate("user", { _id: 0, email: 1 })
+    res
+      .status(200)
+      .json({ message: "Note successfully updated", data: updatedNote })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete("/:noteId", async (req, res, next) => {
+  const noteId = req.params.noteId
+  try {
+    const deletedNote = await Note.findByIdAndDelete(noteId)
+    res.status(200).json({ message: "Note is deleted", data: deletedNote })
   } catch (err) {
     next(err)
   }
