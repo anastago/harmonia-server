e107bd172ceae13898f445d04192e9268375b226
An Go <gorbunoochek@gmail.com>
2024-02-17 16:12:40 +0100

    auth fix

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken")
 const User = require("../models/Users.model")
+const { Long, AutoEncryptionLoggerLevel } = require("mongodb")
 
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers
-
   if (!authorization) {
     return res.status(401).send({ error: "You must be logged in." })
   }
@@ -12,9 +12,10 @@ const requireAuth = (req, res, next) => {
 
   jwt.verify(token, process.env.SECRET_KEY, async (err, payload) => {
     if (err) {
+      console.log(err);
       res.status(401).json({ error: "You must be logged in." })
     }
-
+    console.log(payload)
     const { userId } = payload
     const user = await User.findById(userId)
     req.user = user
