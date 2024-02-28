cd9b097aaf92c153979568b356f260227e820ce2
An Go <gorbunoochek@gmail.com>
2024-02-28 17:39:50 +0100

    model fix

--- models/AIResponses.model.js
+++ models/AIResponses.model.js
@@ -4,13 +4,6 @@ const aiResponseSchema = new Schema({
   text: {
     type: String,
   },
-  createdAt: {
-    type: Date,
-    immutable: true,
-  },
-  updatedAt: {
-    type: Date,
-  },
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note",
